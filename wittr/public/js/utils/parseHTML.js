b7f4673eaae0bbd1d5487dcdbb00d99003f92010
natacha romano <romano.n@hotmail.com>
2018-11-07 15:52:06 -0200

    removing wittr

--- wittr/public/js/utils/parseHTML.js
+++
@@ -1,6 +0,0 @@
-var contextRange = document.createRange();
-contextRange.setStart(document.body, 0);
-
-export default function strToEls(str) {
-  return contextRange.createContextualFragment(str);
-}
